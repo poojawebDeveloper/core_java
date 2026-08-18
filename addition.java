2dd89d73532ce95bf75ae3f47a31f7478189dfcc
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-18 12:59:00 +0530

    Addition program

---
+++ addition.java
@@ -0,0 +1,14 @@
+public class addition{
+int a;
+int b;
+int res;
+public static void main (String args[]){
+addition add = new addition();
+add.a=2;
+add.b=3;
+add.res=add.a+add.b;
+System.out.println(add.a);
+System.out.println(add.b);
+System.out.println(add.res);
+}
+}
