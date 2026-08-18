1a43b669d73d2bf831acbcfe068940e5f49e14e4
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-18 12:32:28 +0530

    fifth commit

---
+++ collage.java
@@ -0,0 +1,11 @@
+public class collage{
+int id;
+String name;
+public static void main (String args[]){
+collage clg = new collage();
+clg.id=101;
+clg.name="BDC COLLAGE";
+System.out.println(clg.id);
+System.out.println(clg.name);
+}
+}
