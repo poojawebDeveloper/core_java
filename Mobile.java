83e255d6f9ab259682bf3a7f45bd5f6a11e70751
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-19 12:55:34 +0530

    MOBILE

---
+++ Mobile.java
@@ -0,0 +1,11 @@
+public class Mobile {
+int id;
+String name;
+ public static void main (String args[]){
+  Mobile m1 =new Mobile();
+m1.id=1;
+m1.name="redami";
+  System.out.println(m1.id);
+  System.out.println(m1.name);
+ }
+}
