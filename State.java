27ef09a5215d4bd66a5060baf9b9a8c79dc625cb
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-19 12:48:20 +0530

    state program

---
+++ State.java
@@ -0,0 +1,22 @@
+public class State{
+int id;
+String name;
+ public static void main (String args[]){
+  State s1 =new State();
+s1.id=1;
+s1.name="Maharashtra";
+  System.out.println(s1.id);
+  System.out.println(s1.name);
+ }
+}
+ class Country{
+int Id;
+String Name;
+ public static void main (String args[]){
+Country c1 = new Country();
+c1.Id=102;
+c1.Name="INDIA";
+   System.out.println(c1.Id);
+  System.out.println(c1.Name);
+ }
+}
