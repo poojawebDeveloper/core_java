14ae010f6663a79e48330013ad809387aed4a759
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-19 12:08:58 +0530

    add to class

---
+++ One.java
@@ -0,0 +1,10 @@
+public class One {
+public static void main(String args[]){
+System.out.println("hello one");
+}
+}
+ class two {
+public static void main(String args[]){
+System.out.println("hello Two");
+}
+}
