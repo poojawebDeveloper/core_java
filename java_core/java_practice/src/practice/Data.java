edb67c9913c5f9778c9518c1273284f7227af9a8
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-21 12:21:12 +0530

    addition

---
+++ java_core/java_practice/src/practice/Data.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Data {
+ int num;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=100;
+System.out.println(num);
+
+	}
+
+}
---
+++ java_core/java_practice/src/practice/Num.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Num {
+  int first;
+  int second;
+  int res;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int first=10;
+int second=20;
+int res=first+second;
+System.out.println("res=" +res);
+	}
+
+}
