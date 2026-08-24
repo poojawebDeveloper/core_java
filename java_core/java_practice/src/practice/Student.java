a772a4702001cb90c71ff70eb12c561815d5816a
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-24 13:07:39 +0530

    data

---
+++ java_core/java_practice/src/practice/Student.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class Student {
+    byte age;
+    short id;
+    int birth;
+    float marks;
+    double per;
+    char gender;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 byte age=22;
+		    short id=127;
+		    int birth=2004;
+		    float marks= 98.45865f;
+		    double per=99.58465647;
+		    char gender='f';
+		    
+		    System.out.println(age);
+		    System.out.println(id);
+		    System.out.println(birth);
+		    System.out.println(marks);
+		    System.out.println(per);
+		    System.out.println(gender);
+	}
+	
+
+}
