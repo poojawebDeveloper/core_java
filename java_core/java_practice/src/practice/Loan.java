660b8d13ee6f627702a082a47e5ba96af042d10b
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-27 12:27:25 +0530

    conditinal

---
+++ java_core/java_practice/src/practice/Loan.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Loan {
+
+	public static void main(String[] args) {
+    double salary=90000.00;
+    
+    if(salary>100000)
+    {
+    	System.out.println("your eligible for the loan");
+    }
+    else
+    {
+    	System.out.println("your not eligible for loan");
+    }
+
+	}
+
+}
