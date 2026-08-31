db1db8eaac044f2e68e027c65d42cfe6a866856f
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-08-31 11:22:18 +0530

    program

---
+++ java_core/java_practice/src/practice/Elseif.java
@@ -0,0 +1,30 @@
+package practice;
+
+public class Elseif {
+
+	public static void main(String[] args) {
+		int num=102;
+		
+		if(num%2==0&&num%3==0) {
+			System.out.println("2,3");
+	        }
+		else if(num%4==0&&num%5==0)
+		{
+			System.out.println("4,5");
+		}
+		else if(num%6==0&&num%7==0)
+		{
+			System.out.println("6,7");
+		}
+		else if(num%8==0&&num%9==0)
+		{
+			System.out.println("8,9");
+		}
+		else
+		{
+			System.out.println("no pair available");
+		}
+	}
+	
+
+}
--- java_core/java_practice/src/practice/Grade.java
+++ java_core/java_practice/src/practice/Grade.java
@@ -3,27 +3,27 @@ package practice;
 public class Grade {
 
 	public static void main(String[] args) {
-		int marks =85;
-      if(marks>=90) {
-    	  System.out.println("Grade A");
-      }
-      else if(marks>=80) {
-    	  System.out.println("Grade B");
-      }
-      else if(marks>=70) {
-    	  System.out.println("Grade C");
-      }
-      else if(marks>=60) {
-    	  System.out.println("Grade D");
-      }
-      else if(marks>=50) {
+		int marks =125;
+      if(marks>=35&&marks<=50) {
     	  System.out.println("Grade E");
       }
-      else if(marks>=35) {
-    	  System.out.println("Pass");
+      else if(marks>=51&&marks<=60) {
+    	  System.out.println("Grade D");
+      }
+      else if(marks>=61&&marks<=70) {
+    	  System.out.println("Grade C");
+      }
+      else if(marks>=71&&marks<=80) {
+    	  System.out.println("Grade B");
+      }
+      else if(marks>=81&&marks<=90) {
+    	  System.out.println("Grade A");
+      }
+      else if(marks>=91&&marks<=100) {
+    	  System.out.println("A+");
       }
       else {
-    	  System.out.println("Fail");
+    	  System.out.println("Fail & invalid marks");
       }
 	}
 
