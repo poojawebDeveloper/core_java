5117c1cf8600162a2eb63d68d73f44b4ca9342d3
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-04 11:51:30 +0530

    loop

---
+++ java_core/java_practice/src/loop/dowhile_table.java
@@ -0,0 +1,37 @@
+package loop;
+
+public class dowhile_table {
+
+	public static void main(String[] args) {
+      /*int num=2;
+      int sum=1;
+      do
+      {
+    	  System.out.println(sum*num);
+    	  sum++;
+      }while(sum<=10);*/
+		
+		/*int num=5;
+		int sum=1;
+		while(sum<=10)
+		{
+			System.out.println(sum*num);
+			sum++;
+		}*/
+		int j=3;
+		int sum=1;
+		int sum1=1;
+		for(int i=2;sum<=10;)
+		{
+			System.out.println(i*sum);
+			sum++;
+		}
+			for( j=3;sum1<=10;)
+			{
+				System.out.println(j*sum1);
+				sum1++;
+			}
+		}
+	}
+
+
