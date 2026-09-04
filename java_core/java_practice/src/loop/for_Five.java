dce39c496e1be71804b7b81386e8eea41ff1d564
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-04 11:59:32 +0530

    five

---
+++ java_core/java_practice/src/loop/for_Five.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class for_Five {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<=10;i++)
+		{
+			if(i==5) 
+			{
+			  System.out.println("FIVE");	
+			}
+			System.out.println(i);
+		}
+	}
+
+}
