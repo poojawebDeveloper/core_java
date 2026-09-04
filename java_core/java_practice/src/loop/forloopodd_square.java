523c1aec6b75f245e714747b9a7a2bad22dfa81a
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-04 12:07:43 +0530

    program

---
+++ java_core/java_practice/src/loop/forloopodd_square.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class forloopodd_square {
+
+	public static void main(String[] args) {
+    
+		int i;
+		for(i=1;i<=100;i++)
+		{
+			if(i%2!=0)
+			{	
+			System.out.println(i);
+			System.out.println(i*i);
+			}
+		}
+	}
+
+}
