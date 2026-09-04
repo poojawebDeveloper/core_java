ade8da7f3077684e058e2dc56e5f2dd27e81956a
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-04 11:52:49 +0530

    loop

---
+++ java_core/java_practice/src/loop/loop_break.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class loop_break {
+
+	public static void main(String[] args) {
+     for(int i=1;i<=20;i++)
+     {
+    	 if(i==6)
+    	 {
+    		 break;
+    	 }
+    	 System.out.println(i);
+     }
+	}
+
+}
