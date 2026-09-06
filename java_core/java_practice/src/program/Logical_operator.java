53dd2c9948896c7119b46d6bfa7f986d41e2e70f
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-06 21:31:28 +0530

    operator

---
+++ java_core/java_practice/src/program/Logical_operator.java
@@ -0,0 +1,13 @@
+package program;
+
+public class Logical_operator {
+
+	public static void main(String[] args) {
+		
+       System.out.println(true&&false||false&&true||true&&false||true);//true
+       
+       System.out.println(false||true&&true||false&&true||false );//true
+       
+	}
+
+}
