d2d518e98a3cedc15be79f6b57a05303b800dcd2
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-06 21:27:26 +0530

    program

---
+++ java_core/java_practice/src/program/if_elseif_else.java
@@ -0,0 +1,26 @@
+package program;
+
+public class if_elseif_else {
+
+	public static void main(String[] args) {
+    
+		int num=5;
+		
+		if(num%2==0)
+		{
+			System.out.println("thie is even number");
+		}
+		else if(num%2!=0)
+		{
+			System.out.println("this is odd number");
+		}
+		else
+		{
+			System.out.println("this is prime number");
+		}
+		
+		
+		
+	}
+
+}
