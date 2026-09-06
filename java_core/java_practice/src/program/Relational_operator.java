ced469ee0449c62f211823b7fce7a43d349405aa
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-06 21:33:55 +0530

    relational

---
+++ java_core/java_practice/src/program/Relational_operator.java
@@ -0,0 +1,17 @@
+package program;
+
+public class Relational_operator {
+
+	public static void main(String[] args) {
+     int num=60;
+     int num1=80;
+     
+     System.out.println(num==num1);//false
+     System.out.println(num!=num1);//true
+     System.out.println(num>num1);//false
+     System.out.println(num<num1);//true
+     System.out.println(num>=num1);//false
+     System.out.println(num<=num1);//true
+	}
+
+}
