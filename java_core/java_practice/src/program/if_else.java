5833d6f739a19d1c3b475625c73f742d5fb4b95b
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-06 21:26:34 +0530

    program

---
+++ java_core/java_practice/src/program/if_else.java
@@ -0,0 +1,19 @@
+package program;
+
+public class if_else {
+
+	public static void main(String[] args) {
+
+		int age=21;
+		
+		if(age>18)
+		{
+			System.out.println("eligible for vote");
+		}
+		else
+		{
+			System.out.println("not eligible for vote");
+		}
+	}
+
+}
