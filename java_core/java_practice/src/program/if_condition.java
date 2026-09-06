abdc6be70dc127aab4df266e5ffe5ba9c8a8005d
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-06 21:29:39 +0530

    if

---
+++ java_core/java_practice/src/program/if_condition.java
@@ -0,0 +1,16 @@
+package program;
+
+public class if_condition {
+	
+	public static void main (String args[]) {
+     
+		int age=8;
+		if(age<=18)
+		{
+			System.out.println("child");
+		}
+		
+		
+		
+	}
+}
