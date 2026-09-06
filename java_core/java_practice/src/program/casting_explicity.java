17b4c78e86c16b15743695eaf9ff57ce68e7ea5c
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-06 21:35:28 +0530

    casting type

---
+++ java_core/java_practice/src/program/casting_explicity.java
@@ -0,0 +1,17 @@
+package program;
+
+public class casting_explicity {
+
+	public static void main(String[] args) {
+
+		double d=40.56;
+		int a=(int)d;
+		System.out.println(a);
+		
+		
+		short s=32767;
+		int i=s;
+		System.out.println(i);
+	}
+
+}
