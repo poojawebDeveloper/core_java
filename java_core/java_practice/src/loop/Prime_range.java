53bb1275008f61213bf29516d0338c344b24ebbc
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-07 13:36:28 +0530

    program

---
+++ java_core/java_practice/src/loop/Prime_range.java
@@ -0,0 +1,33 @@
+package loop;
+
+public class Prime_range {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<20;i++)
+		{
+			boolean flag=false;
+			for(int j=2;j<i;j++)
+			{
+				if(i==2)
+				{
+					continue;
+				}
+				if(i%j==0)
+				{
+					flag=true;
+				}
+			}
+			if(!flag)
+			{
+				System.out.println("prime:"+i);
+			}
+			else
+			{
+				System.out.println("composite");
+			}
+			
+		}
+	}
+
+}
