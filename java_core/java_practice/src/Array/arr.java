393605b9e8ff2565cd233afa2456a78125f61d25
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-08 11:42:33 +0530

    array

---
+++ java_core/java_practice/src/Array/arr.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class arr {
+
+	public static void main(String[] args) {
+
+		int [] arr = new int[5];
+		
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		
+		System.out.println(arr[0]+2);
+		System.out.println(arr[1]*3);
+		System.out.println(arr[2]-4);
+		System.out.println(arr[3]/6);
+		System.out.println(arr[4]%2);
+
+		
+		
+		
+	}
+
+}
