c7269002da7cd11e0cd7420d564a9a646bf18fd8
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-08 10:04:33 +0530

    prime

--- java_core/java_practice/src/loop/Forloop_prime1to50.java
+++ java_core/java_practice/src/loop/Forloop_prime1to50.java
@@ -4,10 +4,21 @@ public class Forloop_prime1to50 {
 
 	public static void main(String[] args) {
 
-		for(int i=1;i<=50;i++)
+		for(int i=3;i<=50;i++)
 		{
-			if
-			System.out.println(i);
+			boolean flag=false;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					flag=true;
+				}
+			}
+			if(!flag)
+			{
+				System.out.println("prime:"+i);
+			}
+						
 		}
 	}
 
