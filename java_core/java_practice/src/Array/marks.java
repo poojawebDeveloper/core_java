dc7b5b4b6c7bf53284050432c88f19aedac8a220
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-08 11:49:23 +0530

    array

---
+++ java_core/java_practice/src/Array/marks.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class marks {
+
+	public static void main(String[] args) {
+
+		//int marks[]=new int [5];
+		
+		int marks[]= {78,86,59,86,46};
+		
+		System.out.println(marks.length);
+		System.out.println(marks[2]);
+		
+	}
+
+}
