6eb5b22f85e25c71ab80d11bd19bda6ef225ce9f
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-08 12:11:17 +0530

    counter

---
+++ java_core/java_practice/src/loop/nested_for_loop.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class nested_for_loop {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<5;i++)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.println("*");
+			}
+		}
+	}
+
+}
