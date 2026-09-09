40f8995272e1a337360e71889630bbb7e8c787e2
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-09 12:17:53 +0530

    prime

---
+++ java_core/java_practice/src/loop/prime_program_8.java
@@ -0,0 +1,31 @@
+package loop;
+
+import java.util.Scanner;
+
+public class prime_program_8 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+     for(int i=n;i<=n1;i++)
+     {
+    	 boolean flag=false;
+    	 for(int j=2;j<i;j++)
+    	 {
+    		 if(i%j==0)
+    		 {
+    			 flag=true;
+    			 break;
+    		 }
+    	 }
+    	 if(!flag)
+    	 {
+    		 System.out.println("prime:"+i);
+    	 }
+     }
+	}
+
+}
