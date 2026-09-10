766b44663a36a40a3eb75e36ab8a452066785d28
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-10 14:07:08 +0530

    prime

---
+++ java_core/java_practice/src/program/program_prime.java
@@ -0,0 +1,30 @@
+package program;
+
+public class program_prime {
+
+	public static void main(String[] args) {
+
+       int num=3;
+       int counter=0;
+       while(counter<100)
+       {
+    	   boolean flag=false;
+    	   for(int i=2;i<num;i++)
+    	   {
+    		   if(num%i==0)
+    		   {
+    			   flag=true;
+    			   break;
+    		   }
+    	   }
+    	   if(!flag)
+    	   {
+    		   System.out.println("prime:"+num);
+    		   counter++;
+    	   }
+    	   num++;
+       }
+       System.out.println("counter:"+counter);
+	}
+
+}
