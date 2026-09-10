a5de7be10a045409efaf185e0dfdf56da12914cc
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-10 13:52:39 +0530

    table

---
+++ java_core/java_practice/src/program/mul_table.java
@@ -0,0 +1,17 @@
+package program;
+
+public class mul_table {
+
+	public static void main(String[] args) {
+
+		int num=2;
+		int sum=1;
+		
+		while(sum<=10)
+		{
+			System.out.println(num*sum);
+			sum++;
+		}
+		
+	}
+}
