fb0f4b57a95dde824adb01a9cd77b567fcbf0ff5
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-10 14:32:00 +0530

    switch

---
+++ java_core/java_practice/src/program/switch_case.java
@@ -0,0 +1,48 @@
+package program;
+
+import java.util.Scanner;
+
+public class switch_case {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("enter number");
+		int n=sc.nextInt();
+
+    
+    switch(n) {
+    
+    case 1:
+    	System.out.println("Mon");
+    break;
+    case 2:
+    	System.out.println("Tue");
+    break;
+    
+    case 3:
+    	System.out.println("Wed");
+    break;
+    
+    case 4:
+    	System.out.println("Thurs");
+    break;
+    
+    case 5:
+    	System.out.println("Fri");
+    break;
+    
+    case 6:
+    	System.out.println("Sat");
+    break;
+    
+    case 7:
+    	System.out.println("sun");
+    break;
+    
+    default:
+    	System.out.println("invalid day");
+    }
+    
+	}
+
+}
