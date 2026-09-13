def2e6dac561e4a79359c7baa8049466ca5989c5
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-13 16:29:43 +0530

    while

---
+++ java_core/java_practice/src/program/while_square.java
@@ -0,0 +1,18 @@
+package program;
+
+public class while_square {
+
+	public static void main(String[] args) {
+
+     int num=1;
+     int sum=0;
+     while(num<=20)
+     {
+    	 sum=num*num;
+    	 System.out.println(num +"<-" +sum);
+
+    	 num++;
+     }
+	}
+
+}
