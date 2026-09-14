4061e569fcd3b9424110b20c24e391adf34fe35e
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-14 10:30:49 +0530

    array

--- java_core/java_practice/src/Array/array_prime.java
+++ java_core/java_practice/src/Array/array_prime.java
@@ -3,32 +3,31 @@ package Array;
 public class array_prime {
 
 	public static void main(String[] args) {
+		        
+        int[] arr = {12, 7, 15, 18, 23, 30, 11, 40};
+ 
+		//int[] arr = {11, 20, 7, 14, 9, 17, 30, 13};
 
-		int[] arr = {17, 19, 5, 32, 23, 5,  22, 29, 31, 40, 43};
-		boolean flag=false;
-		
+    
+    for(int i=0;i<arr.length;i++)
+    {
+    	boolean flag=false;
+    	for(int j=2;j<i;j++)
+    	{
+    		if(arr[i]%j==0)
+    		{
+    			flag=true;
+    			break;
+    		}
+    		if(!flag)
+    		{
+    			System.out.println("prime:"+arr[i]);
+    		}
 
-		for( int i=0;i<arr.length;i++)
-	     {
-			for(int j=2;j<i;j++)
-			{
-				
-			if(arr[i]%j==0)
-			{
-				flag=true;
-				
-			}
-			}
-		
-			if(!flag)
-			{
-				System.out.println("prime:"+arr[i]);
-			}
-		}
-		System.out.println(arr.length);
-				
-		}
-
-	}
+    	}
+    }
 
 
+
+}
+}
--- java_core/java_practice/src/Array/array_program4.java
+++ java_core/java_practice/src/Array/array_program4.java
@@ -4,7 +4,7 @@ public class array_program4 {
 
 	public static void main(String[] args) {
 
-		int[] arr = {12, 7, 15, 18, 23, 30, 11, 40,50,99,87,55};
+		int[] arr = {12,7,15,18,23,30,11,40,50,99,87,55};
         int counter1=0;
         int counter2=0;
 		for(int i=0;i<arr.length;i++)
