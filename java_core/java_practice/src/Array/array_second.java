e87a7686f6c7dd31d6e1985a20a2ab0caabc23d9
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-16 13:42:27 +0530

    array

--- java_core/java_practice/src/Array/array_second.java
+++ java_core/java_practice/src/Array/array_second.java
@@ -6,17 +6,24 @@ public class array_second {
        
 		int[] arr = {12, 7, 15, 18, 23, 30, 11, 40};
         int lowest=0;
-        
+        int slowest=0;
         for(int i=0;i<arr.length;i++)
         {
         	if(arr[i]>lowest)
-        	{
+        	{ 
+        		slowest=lowest;
         		lowest=arr[i];
         	
         	}
+        	if(arr[i]!=lowest&&arr[i]>slowest)
+        	{
+        		slowest=arr[i];
+        	}
+        	
         }
-        System.out.println("heights:"+lowest);
-       
+        System.out.println("first hieghts:"+lowest);
+        System.out.println("second hieghts:"+slowest);
+
 	}
 
 }
