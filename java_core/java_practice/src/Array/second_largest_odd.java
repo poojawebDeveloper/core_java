45b55619de0185b273c7cd9bff71d1c2e6fede0c
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-16 16:03:52 +0530

    array

---
+++ java_core/java_practice/src/Array/second_largest_odd.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class second_largest_odd {
+
+	public static void main(String[] args) {
+
+	int arr[]= {64, 17, 92, 8, 45, 71, 23, 56, 99, 31, 14, 83, 38, 5, 76};
+	int heights=32768;
+	int slowest=32768;
+	for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]%2!=0)
+		{
+			System.out.println("ood number array:"+arr[i]);
+		}
+	
+		if(arr[i]<heights)
+		{
+			slowest=heights;
+			heights=arr[i];
+		}
+		if(arr[i]!=heights&&arr[i]<slowest)
+		{
+			slowest=arr[i];
+		}
+	}
+
+    System.out.println("1lowest array odd:"+heights);
+    System.out.println("2lowest array odd:"+slowest);
+
+	}
+
+}
