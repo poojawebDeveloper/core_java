c2407b1878458fea432d1b03d3151f124f5c1ff2
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-16 14:28:38 +0530

    array

--- java_core/java_practice/src/Array/find_even.java
+++ java_core/java_practice/src/Array/find_even.java
@@ -11,10 +11,30 @@ public class find_even {
 		for(int i=0;i<arr.length;i++)
 		{
 			
-			if(arr[i]%2==0)
+			if(i%2==0)
 			{
 				System.out.println(arr[i]);
 			}
+			boolean flag=false;
+			for(int j=2;j<arr[i];j++)
+			{
+				if(arr[i]%j==0)
+				{
+					flag=true;
+					break;
+				}
+				
+			}
+			if(!flag)
+			{
+				System.out.println("even prime:"+arr[i]);
+				if(i%2!=0)
+				{
+					System.out.println("normal index:"+arr[i]);
+				}
+			}
+
+
 		}
 	}
 
