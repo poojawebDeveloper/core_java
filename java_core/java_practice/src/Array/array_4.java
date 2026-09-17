4ace0d9f60b43306940b83436548eeef5ec2f1de
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-17 11:57:05 +0530

    array

---
+++ java_core/java_practice/src/Array/array_4.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class array_4 {
+
+	public static void main(String[] args) {
+		
+		int arr[][]=new int [3][4];
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				arr[i][j]=10;
+			}
+		}
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+
+				System.out.print(arr[i][j] +" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
