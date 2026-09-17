8528ff7a44beb128126cc3d182c8e1c5372a667c
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-17 16:39:51 +0530

    array

---
+++ java_core/java_practice/src/Array/swap_2variable.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class swap_2variable {
+
+	public static void main(String[] args) {
+
+     int a=10;
+     int b=20;
+     int temp;
+     if(a!=b)
+     {
+    	 temp=a;
+    	 a=b;
+    	 b=temp;
+    	 
+    	 System.out.println("a:"+a);
+    	 System.out.println("b:"+b);
+         System.out.println("t:"+temp);
+
+    	 
+     }
+	}
+
+}
