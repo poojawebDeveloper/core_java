f0529a3835a8c6a4b43b2e8cf2de779d72b19cde
poojawebDeveloper <suryawanshipooja1192@gmail.com>
2026-09-22 12:52:47 +0530

    array

---
+++ java_core/java_practice/src/Array_2D/array_1.java
@@ -0,0 +1,43 @@
+package Array_2D;
+
+public class array_1 {
+
+	public static void main(String[] args) {
+
+    int arr[][]=new int [3][3];
+    
+    arr[0][0]=1;
+    arr[0][1]=2;
+    arr[0][2]=3;
+    arr[1][0]=4;
+    arr[1][1]=5;
+    arr[1][2]=6;
+    arr[2][0]=7;
+    arr[2][1]=8;
+    arr[2][2]=9;
+
+   for(int i=0;i<arr.length;i++)
+   {
+	   boolean flag =false;
+	   for(int j=0;j<arr[i].length;j++)
+	   {
+		   //System.out.print(arr[i][j]+" ");
+	   
+	   System.out.println();
+	  for(int k=2;k<arr[i][j];k++)
+	  {
+		  if(arr[i][j]%k==0)
+		  {
+			  flag=true;
+			  break;
+		  }
+	  }
+	  if(!flag)
+	  {
+		  System.out.println("prime"+arr[i][j]);
+	  }
+   }
+    
+	} 		
+	}
+}
